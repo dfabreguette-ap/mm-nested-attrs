87acbe84acf4cd8aa705468b7fc8c5e705899833
David Fabreguette <david@appliserv.fr>
2014-10-09 12:07:30 +0200

    ..

--- lib/mm-nested-attrs-engine.rb
+++ lib/mm-nested-attrs-engine.rb
@@ -3,11 +3,7 @@ require "mm-nested-attrs/version"
 module MmNestedAttrs
   module Rails
     class Engine < ::Rails::Engine
-      config.before_initialize do
-        if config.action_view.javascript_expansions
-          config.action_view.javascript_expansions["mm-nested-attrs"] = %w(mm-nested-attrs)
-        end
-      end
+      
     end
   end
 end
