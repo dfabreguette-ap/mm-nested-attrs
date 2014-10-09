b096bedc635de5504bf6c1c7d09026ab24c35ae8
David Fabreguette <david@appliserv.fr>
2014-10-09 11:56:32 +0200

    ..

---
+++ lib/mm-nested-attrs-engine.rb
@@ -0,0 +1,8 @@
+require "mm-nested-attrs/version"
+
+module MmNestedAttrs
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/mm-nested-attrs/version.rb
@@ -0,0 +1,5 @@
+module MmNestedAttrs
+  module Rails
+    VERSION = "0.0.2"
+  end
+end
